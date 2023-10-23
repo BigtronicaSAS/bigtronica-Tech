ad5337084d210e4f1d5817d375299c78ba57ff79
BigtronicaSAS <sistemasysoporte@bigtronica.com>
2023-10-23 11:53:44 -0500

    page como comprar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import IndexPage from './pages/IndexPage/IndexPage';
 import ContactPage from './pages/ContactPage/ContactPage';
+import BuyPage from './pages/BuyPage/BuyPage';
 
 function App() {
   return (
     <Routes>
      <Route path="/" element={<IndexPage />}/>
      <Route path="/contacto" element={<ContactPage />}/>
+     <Route path="/Como-Comprar" element={<BuyPage />}/>
     </Routes>
   );
 }
---
+++ src/pages/BuyPage/BuyPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './BuyPage.css';
+import Footer from '../../components/Footer/Footer';
+
+const BuyPage = () => {
+    return (
+        <div>
+
+            
+         <Footer />   
+         </div>
+
+    )
+}
+
+export default BuyPage
