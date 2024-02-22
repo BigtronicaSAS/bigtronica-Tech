dae7d63ca0c9b267819afdf53838c82c603cdadd
BigtronicaSAS <sistemasysoporte@bigtronica.com>
2024-02-22 16:50:20 -0500

    Vista Vacia

---
+++ src/components/EmptyView/EmptyView.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './EmptyView.css'
+
+const EmptyView =() =>{
+  return (
+    <div className='emptyView-wrap'>
+    <p>Ups! Lamentablemente no tenemos esa referencia</p>
+  </div>
+  )
+}
+
+export default EmptyView
+
--- src/pages/ZonaUnit/ZonaUnit.jsx
+++ src/pages/ZonaUnit/ZonaUnit.jsx
@@ -3,9 +3,12 @@ import ListProducts from "../../components/ListProduct/ListProducts";
 import Unit from "../../data/Unit";
 import "./ZonaUnit.css";
 import SearchBar from "../../components/SearchBar/SearchBar";
+import EmptyView from "../../components/EmptyView/EmptyView";
+import Footer from "../../components/Footer/Footer";
 
 const ZonaUnit = () => {
   const [searchInput, setSearchInput] = useState("");
+  const [resultsFound, setResultsFound] = useState(true);
   const [filteredUnit, setFilteredUnit] = useState(Unit); 
 
   const applyFilters = () => {
@@ -18,6 +21,7 @@ const ZonaUnit = () => {
       );
     }
     setFilteredUnit(updatedList);
+    !updatedList.length ? setResultsFound(false) : setResultsFound(true);
   };
 
   useEffect(() => {
@@ -32,7 +36,7 @@ const ZonaUnit = () => {
       <div className="home_panelList-wrap">
         <div className="home_panel-wrap"></div>
         <div className="home_list-wrap">
-          <ListProducts list={filteredUnit} />
+          {resultsFound ? <ListProducts list={filteredUnit} /> : <EmptyView />}
         </div>
       </div>
     </div>
